cb3bcd5c3537ff65c7b1ff212471b2fb761f8f6e
Camilarugamas <20230248@ricaldone.edu.sv>
2025-06-03 10:55:58 -0600

    segundo commit

--- src/main/java/CamilaRugamas_20230248/Stocks.java
+++ src/main/java/CamilaRugamas_20230248/Stocks.java
@@ -1,6 +1,31 @@
 package CamilaRugamas_20230248;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeMap;
+
 public class Stocks {
-    String NombreProductos;
-    int cantidadStock;
+    public void ListadoProductos(){
+        String NombreProductos;
+        int cantidadStock;
+        TreeMap<String, Integer> productos = new TreeMap<>();
+
+        productos.put("Soda",50);
+        productos.put("Churro",100);
+        productos.put("Pan",100);
+        productos.put("Pasteles",100);
+        productos.put("Pizza",100);
+
+        System.out.println("Lista de Productos: " + productos);
+
+        System.out.println("Actualizar un producto: " + productos);
+        productos.(1, "Gaseosa", 20);
+
+    }
+
+
+
+
+
+
 }
